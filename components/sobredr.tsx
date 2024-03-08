b411d2c68f69c747f93f1f692a492b550435685e
jv-pi <joaotei7@hotmail.com>
2024-03-08 17:11:02 -0400

    assd

--- components/sobredr.tsx
+++ components/sobredr.tsx
@@ -10,6 +10,7 @@ export default function Sobredr() {
         <div className="py-12 md:py-20">
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-justify pb-12 md:pb-20 grid">
+            <Image src={Ele} alt="Ele" />{" "}
             <p className="text-xl text-gray-600 mb-6">
               Sávio Batista é um aluno de destaque de 23 anos na Universidade
               Federal do Rio de Janeiro (UFRJ). Sua notável trajetória acadêmica
@@ -26,19 +27,12 @@ export default function Sobredr() {
               com a excelência na pesquisa principalmente na área
               neuro-endovascular.
             </p>
-            <div style={{ display: "flex", alignItems: "center" }}>
-              <Image
-                className="max-w-3xl mx-auto text-center pb-12 md:pb-16 flex justify-center items-center"
-                src={Ele}
-                alt="Garantia"
-              />
-              <p className="text-xl text-gray-600 mb-6">
-                Além de suas realizações locais, Sávio Batista prepara-se para
-                um novo capítulo em sua carreira. Com um Research Fellow já
-                garantido na Emory University, nos Estados Unidos, após a
-                conclusão de seus estudos na UFRJ.
-              </p>
-            </div>
+            <p className="text-xl text-gray-600 mb-6">
+              Além de suas realizações locais, Sávio Batista prepara-se para um
+              novo capítulo em sua carreira. Com um Research Fellow já garantido
+              na Emory University, nos Estados Unidos, após a conclusão de seus
+              estudos na UFRJ.
+            </p>
             <p className="text-xl text-gray-600 mb-6">
               Seu sucesso na área de pesquisa é reflexo de sua dedicação e
               estudo contínuo. Sávio Batista não apenas acumula conquistas
@@ -47,7 +41,7 @@ export default function Sobredr() {
               oportunidades nacionais e internacionais de trabalho à como
               conseguir alcançar esses sonhos!
             </p>
-            <button className="transform bg-black text-white rounded-full font-medium group p-4 shadow-lg">
+            <button className=" transform bg-black text-white rounded-full font-medium group p-4 shadow-lg">
               <a
                 href="https://metodologiacientifica.eadplataforma.app/curso/metocientifica"
                 target="_blank"
