55d334a793e05b6b04f0600e508e4355f965113c
jv-pi <joaotei7@hotmail.com>
2024-03-07 23:27:39 -0400

    pagessss

--- components/features-blocks.tsx
+++ components/features-blocks.tsx
@@ -14,24 +14,9 @@ export default function FeaturesBlocks() {
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
             <h2 className="h2 mb-4">Sobre a Metodologia Cientifíca</h2>
             <p className="text-xl text-gray-600">
-              O Intensivo de Metas é uma imersão de 10 semanas de aulas ao vivo
-              com o professor Rhanderson Cardoso para você aprender desde o zero
-              como criar um projeto, fazer a estatística e escrever o
-              manuscrito. O formato do curso é dividido em aulas teóricas e
-              práticas para te conduzir a fazer uma revisão sistemática e
-              meta-análise de forma independente. O professor do Intensivo de
-              Metas é o Dr. Rhanderson Cardoso, médico cardiologista formado
-              pela Universidade Federal de Goiás, com residência em clínica
-              médica pela Universidade de Miami e residência em cardiologia pelo
-              Hospital Johns Hopkins, nos EUA. O Dr. Rhanderson também foi
-              fellow e chief fellow em imagem cardíaca na Harvard Medical
-              School, Brigham and Women’s Hospital, onde atualmente atua como
-              cardiologista e professor. O Dr. Rhanderson possui mais de 70
-              publicações indexadas e mais de 1.400 citações. Além disso,
-              diversos alunos já aplicaram o método do Dr. Rhanderson e
-              obtiveram resultados igualmente extraordinários. QUICLIQUE A
-              ATENÇÃO! O Intensivo de Metas não possui filiação às instituições
-              de ensino mencionadas.
+              Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur excepteur sint occaecat
+              cupidatat.
             </p>
           </div>
         </div>
