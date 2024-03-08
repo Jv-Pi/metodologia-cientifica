53e0d3e59d1b954a2b7de9a4700b8f2e8044286d
jv-pi <joaotei7@hotmail.com>
2024-03-08 00:01:05 -0400

    pagessss

--- components/features-blocks.tsx
+++ components/features-blocks.tsx
@@ -2,18 +2,18 @@ export default function FeaturesBlocks() {
   return (
     <section className="relative">
       {/* Section background (needs .relative class on parent and next sibling elements) */}
+      <h2 className="h2 text-center mb-4">Sobre a Metodologia Cientifíca</h2>
 
       <div className="relative max-w-6xl mx-auto px-4 sm:px-6">
         <div className="py-12 md:py-20">
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h2 className="h2 mb-4">Sobre a Metodologia Cientifíca</h2>
-            <p className="text-xl text-gray-600">
+            <p className="text-xl text-gray-600 mb-6">
               Duis aute irure dolor in reprehenderit in voluptate velit esse
               cillum dolore eu fugiat nulla pariatur excepteur sint occaecat
               cupidatat.
             </p>
-            <p className="text-xl text-gray-600">
+            <p className="text-xl text-gray-600 mb-6">
               O Intensivo de Metas é uma imersão de 10 semanas de aulas ao vivo
               com o professor Rhanderson Cardoso para você aprender desde o zero
               como criar um projeto, fazer a estatística e escrever o
@@ -21,7 +21,7 @@ export default function FeaturesBlocks() {
               práticas para te conduzir a fazer uma revisão sistemática e
               meta-análise de forma independente.
             </p>
-            <p className="text-xl text-gray-600">
+            <p className="text-xl text-gray-600 mb-6">
               O professor do Intensivo de Metas é o Dr. Rhanderson Cardoso,
               médico cardiologista formado pela Universidade Federal de Goiás,
               com residência em clínica médica pela Universidade de Miami e
@@ -31,7 +31,7 @@ export default function FeaturesBlocks() {
               atualmente atua como cardiologista e professor. O Dr. Rhanderson
               possui mais de 70 publicações indexadas e mais de 1.400 citações.
             </p>
-            <p className="text-xl text-gray-600">
+            <p className="text-xl text-gray-600 mb-6">
               Além disso, diversos alunos já aplicaram o método do Dr.
               Rhanderson e obtiveram resultados igualmente extraordinários.
               <a
@@ -42,7 +42,7 @@ export default function FeaturesBlocks() {
                 Clica Aqui
               </a>
             </p>
-            <p className="text-xl text-gray-600">
+            <p className="text-xl text-gray-600 mb-6">
               ATENÇÃO! O Intensivo de Metas não possui filiação às instituições
               de ensino mencionadas.
             </p>
