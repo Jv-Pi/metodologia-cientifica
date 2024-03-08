0b218c2d41692327bb434c78e493cac5828bd1c9
jv-pi <joaotei7@hotmail.com>
2024-03-07 22:21:01 -0400

    pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import "./css/style.css";
 
 import { Inter } from "next/font/google";
 
-import Header from "@/components/ui/header";
-
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
@@ -26,7 +24,6 @@ export default function RootLayout({
         className={`${inter.variable} font-inter antialiased bg-white text-gray-900 tracking-tight`}
       >
         <div className="flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip">
-          <Header />
           {children}
         </div>
       </body>
