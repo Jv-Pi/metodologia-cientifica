73f50cc7ada57531de389e580d1eaf2049d9d284
Joao Victor Santos <joaosantos@mti.mt.gov.br>
2024-03-08 09:59:32 -0400

    asd

--- components/features.tsx
+++ components/features.tsx
@@ -173,7 +173,7 @@ export default function Features() {
                           className="hidden duration-700 ease-in-out"
                           data-carousel-item
                         >
-                          <img
+                          <Image
                             src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
                             className="absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                             alt="asd"
@@ -184,7 +184,7 @@ export default function Features() {
                           className="hidden duration-700 ease-in-out"
                           data-carousel-item="active"
                         >
-                          <img
+                          <Image
                             src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
                             className="absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                             alt=""
@@ -195,7 +195,7 @@ export default function Features() {
                           className="hidden duration-700 ease-in-out"
                           data-carousel-item
                         >
-                          <img
+                          <Image
                             src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
                             className="absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                             alt=""
@@ -206,7 +206,7 @@ export default function Features() {
                           className="hidden duration-700 ease-in-out"
                           data-carousel-item
                         >
-                          <img
+                          <Image
                             src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
                             className="absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                             alt=""
@@ -217,7 +217,7 @@ export default function Features() {
                           className="hidden duration-700 ease-in-out"
                           data-carousel-item
                         >
-                          <img
+                          <Image
                             src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg"
                             className="absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                             alt=""
@@ -301,7 +301,7 @@ export default function Features() {
                           className="hidden duration-700 ease-in-out"
                           data-carousel-item
                         >
-                          <img
+                          <Image
                             src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
                             className="absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                             alt="asd"
@@ -312,7 +312,7 @@ export default function Features() {
                           className="hidden duration-700 ease-in-out"
                           data-carousel-item="active"
                         >
-                          <img
+                          <Image
                             src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
                             className="absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                             alt=""
@@ -323,7 +323,7 @@ export default function Features() {
                           className="hidden duration-700 ease-in-out"
                           data-carousel-item
                         >
-                          <img
+                          <Image
                             src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
                             className="absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                             alt=""
@@ -334,7 +334,7 @@ export default function Features() {
                           className="hidden duration-700 ease-in-out"
                           data-carousel-item
                         >
-                          <img
+                          <Image
                             src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
                             className="absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                             alt=""
@@ -345,7 +345,7 @@ export default function Features() {
                           className="hidden duration-700 ease-in-out"
                           data-carousel-item
                         >
-                          <img
+                          <Image
                             src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg"
                             className="absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                             alt=""
