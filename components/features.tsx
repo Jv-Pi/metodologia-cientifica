0632ea61a0bd44472a9eb6b7b6455c31f36b49e6
Joao Victor Santos <joaosantos@mti.mt.gov.br>
2024-03-08 10:57:51 -0400

    asdasd

--- components/features.tsx
+++ components/features.tsx
@@ -3,7 +3,9 @@
 import { useState, useRef, useEffect } from "react";
 import { Transition } from "@headlessui/react";
 import Image from "next/image";
-import { Carousel } from "@material-tailwind/react";
+import Carousel from "react-bootstrap/Carousel";
+import ExampleCarouselImage from "components/ExampleCarouselImage";
+
 export default function Features() {
   const [tab, setTab] = useState<number>(1);
 
@@ -159,26 +161,37 @@ export default function Features() {
                     beforeEnter={() => heightFix()}
                     unmount={false}
                   >
-                    <Carousel
-                      loop={true}
-                      autoplay={true}
-                      className="rounded-xl"
-                    >
-                      <img
-                        src="https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-                        alt="image 1"
-                        className="h-full w-full object-cover object-center"
-                      />
-                      <img
-                        src="https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-                        alt="image 2"
-                        className="h-full w-full object-cover object-center"
-                      />
-                      <img
-                        src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-                        alt="image 3"
-                        className="h-full w-full object-cover object-center"
-                      />
+                    <Carousel fade>
+                      <Carousel.Item>
+                        <ExampleCarouselImage text="First slide" />
+                        <Carousel.Caption>
+                          <h3>First slide label</h3>
+                          <p>
+                            Nulla vitae elit libero, a pharetra augue mollis
+                            interdum.
+                          </p>
+                        </Carousel.Caption>
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <ExampleCarouselImage text="Second slide" />
+                        <Carousel.Caption>
+                          <h3>Second slide label</h3>
+                          <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit.
+                          </p>
+                        </Carousel.Caption>
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <ExampleCarouselImage text="Third slide" />
+                        <Carousel.Caption>
+                          <h3>Third slide label</h3>
+                          <p>
+                            Praesent commodo cursus magna, vel scelerisque nisl
+                            consectetur.
+                          </p>
+                        </Carousel.Caption>
+                      </Carousel.Item>
                     </Carousel>
                   </Transition>
                   {/* Item 2 */}
