b81203d0a0b9b47086a96cae323e68a93b5962e7
Joao Victor Santos <joaosantos@mti.mt.gov.br>
2024-03-08 10:26:50 -0400

    asdasd

--- components/features.tsx
+++ components/features.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useRef, useEffect } from "react";
 import { Transition } from "@headlessui/react";
+
 import Image from "next/image";
 import FeaturesBg from "@/public/images/features-bg.png";
 import FeaturesElement from "@/public/images/features-element.png";
