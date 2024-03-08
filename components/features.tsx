24d6d0036d1330856f33bc5a4487188709c0ac2e
jv-pi <joaotei7@hotmail.com>
2024-03-08 00:27:09 -0400

    pagessss

--- components/features.tsx
+++ components/features.tsx
@@ -170,21 +170,10 @@ export default function Features() {
                     unmount={false}
                   >
                     <div className="relative inline-flex flex-col">
-                      <Image
-                        className="md:max-w-none mx-auto rounded"
-                        src={FeaturesBg}
-                        width={500}
-                        height="462"
-                        alt="Features bg"
-                      />
-                      <Image
-                        className="md:max-w-none absolute w-full left-0 transform animate-float"
-                        src={FeaturesElement}
-                        width={500}
-                        height="44"
-                        alt="Element"
-                        style={{ top: "30%" }}
-                      />
+                    <div class="grid grid-rows-2 grid-flow-col">
+                      <div>01</div>
+                      <div>09</div>
+                    </div>
                     </div>
                   </Transition>
                   {/* Item 2 */}
