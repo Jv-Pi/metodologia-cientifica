4609da69412431705a52ed2e582f0e68b6ce1a92
jv-pi <joaotei7@hotmail.com>
2024-03-08 17:50:19 -0400

    assd

--- components/sobredr.tsx
+++ components/sobredr.tsx
@@ -10,7 +10,10 @@ export default function Sobredr() {
         <div className="py-12 md:py-20">
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-justify pb-12 md:pb-20 justify-items-center grid">
-            <p className="text-xl max-w-3xl mx-auto flex text-gray-600 mb-6">
+            <p
+              className="ImagemResponsiva"
+              className="text-xl max-w-3xl mx-auto flex text-gray-600 mb-6"
+            >
               Sávio Batista é um aluno de destaque de 23 anos na Universidade
               Federal do Rio de Janeiro (UFRJ). Sua notável trajetória acadêmica
               e profissional destaca-o como um especialista em publicações
