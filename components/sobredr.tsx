a7eeaca34da8d4e7aff9a28bec61092c47b09bc0
jv-pi <joaotei7@hotmail.com>
2024-03-08 16:49:03 -0400

    asd

--- components/sobredr.tsx
+++ components/sobredr.tsx
@@ -9,50 +9,51 @@ export default function Sobredr() {
       <div className="relative max-w-6xl mx-auto px-4 sm:px-6">
         <div className="py-12 md:py-20">
           {/* Section header */}
-          <div className="max-w-3xl mx-auto text-justify pb-12 md:pb-20 grid">
-            <Image
-              className="max-w-3xl mx-auto text-center pb-12 md:pb-16 flex justify-center items-center"
-              src={Ele}
-              alt="Garantia"
-            />
-            <p className="text-xl text-gray-600 mb-6">
-              Sávio Batista é um aluno de destaque de 23 anos na Universidade
-              Federal do Rio de Janeiro (UFRJ). Sua notável trajetória acadêmica
-              e profissional destaca-o como um especialista em publicações
-              científicas, com mais de 50 artigos publicados em periódicos
-              indexados.
-            </p>
-            <p className="text-xl text-gray-600 mb-6">
-              Ao longo de sua jornada acadêmica, Sávio tem se destacado não
-              apenas por suas realizações individuais, mas também por sua
-              contribuição significativa ao MB Research Group, um grupo de
-              pesquisa brasileiro. Sua participação ativa neste grupo tem
-              fortalecido a comunidade científica, evidenciando seu compromisso
-              com a excelência na pesquisa principalmente na área
-              neuro-endovascular.
-            </p>
-            <p className="text-xl text-gray-600 mb-6">
-              Além de suas realizações locais, Sávio Batista prepara-se para um
-              novo capítulo em sua carreira. Com um Research Fellow já garantido
-              na Emory University, nos Estados Unidos, após a conclusão de seus
-              estudos na UFRJ.
-            </p>
-            <p className="text-xl text-gray-600 mb-6">
-              Seu sucesso na área de pesquisa é reflexo de sua dedicação e
-              estudo contínuo. Sávio Batista não apenas acumula conquistas
-              acadêmicas, mas também busca compartilhar seu conhecimento e
-              ensinar todos que querem ter um currículo de impacto, melhores
-              oportunidades nacionais e internacionais de trabalho à como
-              conseguir alcançar esses sonhos!
-            </p>
-            <button className=" transform bg-black text-white rounded-full font-medium group p-4 shadow-lg">
-              <a
-                href="https://metodologiacientifica.eadplataforma.app/curso/metocientifica"
-                target="_blank"
-              >
-                <span className="ml-3">Quero Fazer Parte do Time </span>
-              </a>
-            </button>
+          <div className="max-w-3xl mx-auto text-justify pb-12 md:pb-20 grid md:grid-cols-2 gap-x-8 items-center">
+            <div className="text-center pb-12 md:pb-16 flex justify-center items-center">
+              <Image className="max-w-full h-auto" src={Ele} alt="Garantia" />
+            </div>
+            <div>
+              <p className="text-xl text-gray-600 mb-6">
+                Sávio Batista é um aluno de destaque de 23 anos na Universidade
+                Federal do Rio de Janeiro (UFRJ). Sua notável trajetória
+                acadêmica e profissional destaca-o como um especialista em
+                publicações científicas, com mais de 50 artigos publicados em
+                periódicos indexados.
+              </p>
+              <p className="text-xl text-gray-600 mb-6">
+                Ao longo de sua jornada acadêmica, Sávio tem se destacado não
+                apenas por suas realizações individuais, mas também por sua
+                contribuição significativa ao MB Research Group, um grupo de
+                pesquisa brasileiro. Sua participação ativa neste grupo tem
+                fortalecido a comunidade científica, evidenciando seu
+                compromisso com a excelência na pesquisa principalmente na área
+                neuro-endovascular.
+              </p>
+              <p className="text-xl text-gray-600 mb-6">
+                Além de suas realizações locais, Sávio Batista prepara-se para
+                um novo capítulo em sua carreira. Com um Research Fellow já
+                garantido na Emory University, nos Estados Unidos, após a
+                conclusão de seus estudos na UFRJ.
+              </p>
+              <p className="text-xl text-gray-600 mb-6">
+                Seu sucesso na área de pesquisa é reflexo de sua dedicação e
+                estudo contínuo. Sávio Batista não apenas acumula conquistas
+                acadêmicas, mas também busca compartilhar seu conhecimento e
+                ensinar todos que querem ter um currículo de impacto, melhores
+                oportunidades nacionais e internacionais de trabalho à como
+                conseguir alcançar esses sonhos!
+              </p>
+              <button className="transform bg-black text-white rounded-full font-medium group p-4 shadow-lg">
+                <a
+                  href="https://metodologiacientifica.eadplataforma.app/curso/metocientifica"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <span className="ml-3">Quero Fazer Parte do Time </span>
+                </a>
+              </button>
+            </div>
           </div>
         </div>
       </div>
